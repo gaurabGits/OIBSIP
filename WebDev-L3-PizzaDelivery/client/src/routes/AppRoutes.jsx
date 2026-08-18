84714613d07d9d254119f94c1237f9d3d61dd1bd
Gaurab lohar <gaurablohar@gmail.com>
2026-08-18 23:53:59 +0545

    upt:adding new routes

--- WebDev-L3-PizzaDelivery/client/src/routes/AppRoutes.jsx
+++ WebDev-L3-PizzaDelivery/client/src/routes/AppRoutes.jsx
@@ -1,8 +1,23 @@
 import LandingPage from "../pages/public/LandingPage";
+import SignupPage from './../pages/auth/Signup';
+import LoginPage from './../pages/auth/Login';
+import AboutPage from './../pages/public/AboutPage';
 
 export const routes = [
     {
         path: "/",
         element: <LandingPage />
     },
+    {
+        path: "/about",
+        element: <AboutPage />
+    },
+    {
+        path: "/login",
+        element: <LoginPage />
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />
+    }
 ]
