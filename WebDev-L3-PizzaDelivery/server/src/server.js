94d1c84fe292fa46700bb3b3af7c13635272a9a7
Gaurab lohar <gaurablohar@gmail.com>
2026-09-13 23:13:53 +0545

    fix: imporve code

--- WebDev-L3-PizzaDelivery/server/src/server.js
+++ WebDev-L3-PizzaDelivery/server/src/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on the ${PORT} port`); 
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 startLowStrockJob();
