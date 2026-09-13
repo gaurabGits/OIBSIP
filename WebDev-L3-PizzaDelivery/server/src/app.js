8c7582f3931ddb92e3af91a792dd43fefacbb2fd
Gaurab lohar <gaurablohar@gmail.com>
2026-09-13 22:05:10 +0545

    fix backend routing

--- WebDev-L3-PizzaDelivery/server/src/app.js
+++ WebDev-L3-PizzaDelivery/server/src/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://slicepizzahouse.vercel.app/",
+      "https://pizzaslice4u.vercel.app/",
     ],
     credentials: true,
   })
