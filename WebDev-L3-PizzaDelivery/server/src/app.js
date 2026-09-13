9f22a56158c8b58268bf5fe930fa6fb0c6eaa4ef
Gaurab lohar <gaurablohar@gmail.com>
2026-09-13 20:04:29 +0545

    feat(fix): backend url in deployment

--- WebDev-L3-PizzaDelivery/server/src/app.js
+++ WebDev-L3-PizzaDelivery/server/src/app.js
@@ -14,6 +14,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://slicepizzahouse.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
     res.json({
